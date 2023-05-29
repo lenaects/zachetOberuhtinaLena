fe8ea0ea14f94b705612ca4c025d19f2e1902c29
lenaects <124179210+lenaects@users.noreply.github.com>
2023-05-29 12:01:09 +0500

    Add files via upload

---
+++ zachet/Form1.Designer.cs
@@ -0,0 +1,203 @@
+﻿
+namespace zachet
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose (bool disposing)
+        {
+            if ( disposing && (components != null) )
+            {
+                components.Dispose( );
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent ()
+        {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(58, 48);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(511, 350);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.button4);
+            this.tabPage1.Controls.Add(this.button1);
+            this.tabPage1.Controls.Add(this.listBox1);
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Controls.Add(this.textBox1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(503, 324);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "задание А";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.button5);
+            this.tabPage2.Controls.Add(this.button3);
+            this.tabPage2.Controls.Add(this.button2);
+            this.tabPage2.Controls.Add(this.listBox2);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(503, 324);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "задание Б";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(30, 57);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(155, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(30, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(81, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "введите слово";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(30, 135);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(302, 134);
+            this.listBox1.TabIndex = 2;
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(16, 51);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(231, 173);
+            this.listBox2.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(30, 93);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(155, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "найти число вхождений";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(297, 93);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(170, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "вывести число и частота";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(297, 142);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(170, 23);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "вывести число*частоту";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(359, 218);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(125, 51);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "посмотреть содержимое файлв";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(16, 243);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(125, 51);
+            this.button5.TabIndex = 3;
+            this.button5.Text = "посмотреть содержимое файла";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+    }
+}
+
---
+++ zachet/Form1.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace zachet
+{
+    public partial class Form1 :Form
+    {
+        List <ZadA> zada=new List<ZadA>();
+        List<ZadB> zadb = new List<ZadB>( );
+        public Form1 ()
+        {
+            InitializeComponent( );
+            
+            
+        }
+
+        private void Form1_Load (object sender, EventArgs e)
+        {
+
+        }
+        //чтение из файла строки
+        public string FilRread(string file)
+        {
+                StreamReader sr = File.OpenText("text.txt");
+                while ( !sr.EndOfStream )
+                {
+
+                     file = sr.ReadLine( );
+                    ZadA a = new ZadA( );
+                    a.file = file;
+                    zada.Add(a);
+
+                }
+                sr.Close();
+            return file;
+          
+        }
+        //проверка строки
+        static bool Chek( string stroca)
+        {
+            bool chec = false;
+            //преоброзование символов в строку
+            stroca = stroca.ToLower( );
+                for(int i=0;i<stroca.Length;i++ )
+                {
+                char c =stroca [ i ];
+                if ( c >= 'а' && c <= 'я' ) chec = true;
+                
+                
+                }
+     return chec;
+           
+        }
+        //ишем слова которые совпадают в файле
+        static int poisc (string stroca, string file)
+        {
+
+            List<string> list = file.Split(' ').ToList( );
+            //берём значение из листа
+            // IEnumerable возврашет сылку на другой интерфейс для перебора объектов в foreach
+            IEnumerable<string> result =from s in list
+                    //ищем совпадение слова с словом из файла
+                where s.Contains(stroca)
+                select s;
+            int count = 0;
+            foreach ( string s in result )
+            {
+                count++;
+            }
+            return count;
+        }
+      //добавление в лист  
+        private void button4_Click (object sender, EventArgs e)
+        {
+            string file = "";
+            file = FilRread(file);
+            listBox1.Items.Add(file);
+
+        }
+
+        private void button1_Click (object sender, EventArgs e)
+        {
+            if ( textBox1.TextLength != 0 )
+            {
+                string stroca = textBox1.Text;
+                
+                if ( Chek(stroca) )
+                {                    
+                    string line = "";                   
+                    line  =FilRread (line);
+                    if ( line.Length != 0 )
+                    {
+                        int count = poisc(stroca.ToLower( ), line.ToLower( ));
+                        if ( count > 0 ) MessageBox.Show($"Найдены {count} вхождения(ий) поискового запроса {stroca}");
+                        else MessageBox.Show($"Не найдены входления(ий) поискового запроса {stroca}");
+                    }
+                    else MessageBox.Show("Файл пуст","ОШИБКА");
+                    
+                }
+                else MessageBox.Show("некоректный ввод","ОШИБКА");
+               
+            }
+            else MessageBox.Show("строка не заполненна","ОШИБКА");
+        }
+         //чтение массива из файла 
+        public string MasRead (string file)
+        {
+                StreamReader sr = File.OpenText("mas.txt");
+                while ( !sr.EndOfStream )
+                {
+
+                     file = sr.ReadLine( );
+                    string [ ] s = file.Split(new char [ ] { ';' }, StringSplitOptions.RemoveEmptyEntries);
+
+                    ZadB b = new ZadB( );
+                    //for(int i=0;i<s.Length;i++ )
+                    //{
+                    //   b.mas[i] = int.Parse(s[i]);
+                    //    i++;
+                    //}
+                  
+                    //zadb.Add(b);
+
+                }
+                sr.Close( );
+               return file;
+            
+        }
+
+        private void button5_Click (object sender, EventArgs e)
+        {
+            listBox2.Items.Clear( );
+            string file = "";
+            file = MasRead(file);
+            listBox2.Items.Add(file);
+        }
+       double [ ] mas = { 5.1, 1.3, 9.2, 2.3, 5.1, 3 };
+        private void button2_Click (object sender, EventArgs e)
+        {
+         
+            listBox2.Items.Clear( );
+            var result = mas.GroupBy(x => x)//групируем числа из массива без повторов
+                .Select(x => new { Number = x.Key, Chast = x.Count( ) }) //преобразуем в обьект с двумя свойствами:
+                                                                             //Number значение, Frequency частота
+                .OrderBy(x => x.Number); // сортируем обьект по возростанию
+            listBox1.Items.Add("Числа - Частота");
+            foreach ( var x in result )
+            {
+                string item = $"{x.Number} - {x.Chast}";
+                listBox2.Items.Add(item);
+            }
+        }
+
+        private void button3_Click (object sender, EventArgs e)
+        {
+            listBox2.Items.Clear( );
+            var result = mas.GroupBy(x => x)//групируем числа из массива без повторов
+                .Select(x => new { Number = x.Key, Chast = x.Count( ) })//преобразуем в обьект с двумя свойствами:
+                                                                            //Number значение, Frequency частота
+                .OrderBy(x => x.Number); // сортируем обьект по возростанию
+            listBox1.Items.Add("Число * частоту - Частота");
+            foreach ( var x in result )
+            {
+                string item = $"{x.Number * x.Chast} - {x.Chast}";
+                listBox2.Items.Add(item);
+            }
+        }
+    }
+}
